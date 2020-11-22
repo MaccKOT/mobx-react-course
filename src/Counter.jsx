a5b90d89f4b5b867d4f967cee711457ee5e5949f
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2020-11-22 18:41:27 +0400

    Add mobx lite

---
+++ src/Counter.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Counter() {
+  return (
+    <div className='counter'>
+      <button>+</button>
+      <button>-</button>
+    </div>
+  );
+}
+
+export default Counter;
